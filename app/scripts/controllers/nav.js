ec46ee824db1b4581010843b798f44dd6d90e112
lby3 <1192235026@qq.com>
2017-03-28 09:59:13 +0800

    LBY

--- app/scripts/controllers/nav.js
+++ app/scripts/controllers/nav.js
@@ -4,8 +4,8 @@ $(function(){
 				$(".nav").css({'transform':'translateX(0)'});
 			}
 		})
-		$(".right").on({
-			'touchstart':function(){
+		$(".lby_right").on({
+			'touchend':function(){
 				$("#nav").css({'transform':'translateX(-16rem)'});
 			}
 		})
@@ -15,4 +15,15 @@ $(function(){
 				$(this).addClass('active').siblings().removeClass('active');
 			}
 		})
+		
 })
+window.onscroll=function(){
+	var scrollTop = document.body.scrollTop || document.documentElement.scrollTop;
+	scrollTop=$(document).scrollTop();
+	//console.log(scrollTop);
+	if(scrollTop<=240){
+		$(".lby_top").css("display","none");
+	}else{
+		$(".lby_top").css("display","block");
+	}
+}
